bdd7a62488bcb17921813890ff9c15e6d2c792c6
Amethyst Skye <83793015+amskyepi@users.noreply.github.com>
2022-09-29 15:26:23 -0700

    Delete Programming_Assignments directory

--- Programming_Assignments/assignment1/getHash.c
+++
@@ -1,6 +0,0 @@
-#include "main.h"
-
-NODE **getHT(unsigned long long tablesize){
-    NODE **hashTable = calloc(tablesize, (sizeof(NODE) * tablesize));
-    return hashTable;
-}
--- Programming_Assignments/assignment1/sortFun.c
+++
@@ -1,6 +0,0 @@
-#include "main.h"
-
-int sortFun(const void *x, const void *y){
-    return( *(int*)y - *(int*)x);
-}
-
